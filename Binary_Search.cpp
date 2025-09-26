5835ebdb5f6b767b6cae94843dfb9440be851b95
Tran Thai Bao <tranthaibao334466@gmail.com>
2025-09-26 08:22:07 +0700

    Fixed some errors with the recursive function, but still have problems with return

--- Binary_Search.cpp
+++ Binary_Search.cpp
@@ -4,27 +4,28 @@ using namespace std;
 
 int Binary_Search(int *A, int L, int R, int val)
 {
-    if(L<R)
+    if(L<=R)
     {
         int Mid = (L+R)/2;
         if(val == A[Mid]) return Mid;
         else {
             if(val > A[Mid])
             {
-                Binary_Search(A,Mid+1,R,val);
+                return Binary_Search(A,Mid+1,R,val);
             }
             else
             {
-                Binary_Search(A,L,Mid-1,val);
+                return Binary_Search(A,L,Mid-1,val);
             }
         }
     }
-    return 0; 
+    return __INT_MAX__; 
+    // If your pos = 0, it'll be wrong
 }
 
 int main()
 {
-    /* int *A = new int[100];
+    int *A = new int[100];
     int n;
     int val;
     bool check = true;
@@ -41,12 +42,12 @@ int main()
         cin >> A[i];
     } 
     cout << "Enter the enquiring number: ";
-    cin >> val; */
-    int A[5] = {2,4,5,7,8}; 
-    int val = 7;
-    if(!Binary_Search(A,0,4,val)) {cout << "Can't find the value in the array" ;}
+    cin >> val; 
+    int pos = Binary_Search(A,0,n-1,val); 
+    // Using pos to prevent call function two time ( which is time-wasting)
+    if(pos == __INT_MAX__) {cout << "Can't find the value in the array" ;}
     else{
-        cout << "The position of enquiring number is: " << Binary_Search(A,0,5,val);
+        cout << "The position of enquiring number is: " << pos+1 << endl;
     }
 
 }
