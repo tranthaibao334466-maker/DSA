05e63a1e357cb3a97ab7642941c55cb9021dbf61
Tran Thai Bao <tranthaibao334466@gmail.com>
2025-10-03 09:54:44 +0700

    Complete setting Circular and Doubly Linked List using struct in C

--- Circular_Doubly.c
+++ Circular_Doubly.c
@@ -1,13 +1,19 @@
 // Danh sách liên kết mà ở đó con trỏ next của phần tử cuối danh sách trỏ vào nốt đầu tiên trong danh sách 
 
 #include <stdio.h>
-
+#include <stdlib.h>
 struct Node
 {
     int val;
     struct Node *next;
 };
-void Create_List(struct Node **START, int n)
+struct NODE
+{
+    int val;
+    struct NODE *next;
+    struct NODE *prv;
+};
+void Create_CircularList(struct Node **START, int n)
 {
     int val;
     for(int i=0;i<n;i++)
@@ -30,10 +36,55 @@ void Create_List(struct Node **START, int n)
             else new_node->next = *START;
         }
     }
+}
+void Create_DoublyList(struct NODE **START, int n)
+{
+    int val;
+    for(int i=0;i<n;i++)
+    {
+        printf("Nhap gia tri thu %d : ", i+1);
+        scanf("%d", &val);
+        struct NODE *new_node = (struct NODE*)malloc(sizeof(struct NODE));
+        struct NODE *temp;
+        new_node->next=NULL;
+        new_node->val=val;
+        if(*START == NULL)
+        {
+            *START = new_node;
+            temp = new_node;
+        }
+        else{
+            struct NODE *ptr = *START;
+            while(ptr->next != NULL) ptr = ptr->next;
+            ptr->next = new_node;
+            (ptr->next)->prv = temp;
+            temp = new_node;
+        }
+    }
+}
+// Display Doubly List
 
+// Find an element in Doubly Linked List
+struct NODE *Find_Element(struct NODE *START, int n, int val)
+{
+    struct NODE* ptr = START;
+    while(ptr != NULL)
+    {
+        if(ptr->val == val) return ptr;
+        ptr=ptr->next;
+    }
+    return NULL;
 }
 
 int main()
 { 
+    struct NODE *START = NULL;
+    Create_DoublyList(&START, 5);
+    struct NODE *ptr = Find_Element(START, 5,34);
+    while(ptr != NULL)
+    {
+        printf("\n%d ", ptr->val);
+        ptr = ptr->prv; 
+    }
 
 }
